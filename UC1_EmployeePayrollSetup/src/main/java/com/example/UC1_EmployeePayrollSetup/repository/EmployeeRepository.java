287331b0ba3823cc1f82c60bfa9a1c2183ec0563
Kapil Tiwari <tiwarikapil216@gmail.com>
2025-02-27 11:26:00 +0530

    [Kapil Tiwari] Added UC2_EmployeePayrollSetup

--- UC1_EmployeePayrollSetup/src/main/java/com/example/UC1_EmployeePayrollSetup/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.UC1_EmployeePayrollSetup.repository;
-
-import com.example.UC1_EmployeePayrollSetup.models.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
-}
